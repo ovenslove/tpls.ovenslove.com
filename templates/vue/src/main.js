163367fff29dfa6708e57305f4a456a826a8676d
ovenslove <1905997838@qq.com>
2020-01-03 15:28:22 +0800

    调整vconsole

--- templates/vue/src/main.js
+++ templates/vue/src/main.js
@@ -1,9 +1,12 @@
+const _h = window.location.hash;
+const _s = Symbol();
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import _v from 'vconsole'
+window[_s] = /vc=1/g.test(_h) ? new _v() : null
 Vue.config.productionTip = false
-
 new Vue({
   router,
   store,
